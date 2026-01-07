9fc22a6b338afc9471b7bd3012155ea0d7deadb3
Junko Enoshima <156222018+2dChan@users.noreply.github.com>
2026-01-07 14:34:17 +0300

    fix(fish): prevent fish_plugins overwrite on first fish launch

--- dot-config/fish/config.fish
+++ dot-config/fish/config.fish
@@ -15,12 +15,13 @@ if status is-interactive
     alias ls='lsd --group-directories-first'
 
     # --- Autoinstall fisher and plugins ---
+    set plugins (cat ~/.config/fish/fish_plugins)
+
     if not functions -q fisher
         echo "Install fisher..."
         curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
     end
 
-    set plugins (cat ~/.config/fish/fish_plugins)
     for plugin in $plugins
         if not fisher list | grep -Fq $plugin
             echo "Install plugin: $plugin..."
